845a9caadc2420abd111dee3c9d8405a3ccbdee0
Stella Tóth-Baranyi <stellasipi@gmail.com>
2020-04-03 22:17:23 +0200

    Create basic Hello World app

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func showMessage(sender: UIButton){
+        let alertController=UIAlertController(title:"Welcome to my first IO app! :)", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController,animated: true,completion: nil)
+    }
 
 
 }
